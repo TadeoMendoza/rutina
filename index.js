ad9fa2ec836dd4f13cecff1500a562a76baeb55f
TadeoMendoza <101682587+TadeoMendoza@users.noreply.github.com>
2022-11-04 20:12:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const lunes = document.getElementById("lunes");
+const martes = document.getElementById("martes");
+const miercoles = document.getElementById("miercoles");
+const jueves = document.getElementById("jueves");
+
+lunes.addEventListener('click', () => { 
+    location.href ="/pantallas/lunes.html";
+})
+martes.addEventListener('click', () => { 
+    location.href ="/pantallas/martes.html";
+})
+miercoles.addEventListener('click', () => { 
+    location.href ="/pantallas/miercoles.html";
+})
+jueves.addEventListener('click', () => { 
+    location.href ="/pantallas/jueves.html";
+})
